f5d3f127445ce1784e8b712a122b17a03f13cf77
TestSmirk <d.kc@qq.com>
2019-02-14 17:15:31 +0800

    return json in GetMapping

--- src/main/java/com/example/demo/BookController.java
+++ src/main/java/com/example/demo/BookController.java
@@ -2,16 +2,16 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Date;
 
-@Controller
+@RestController
 public class BookController {
     @GetMapping("/book")
     public Book book() {
-
         Book book = new Book();
         book.setAuthor("罗贯中");
         book.setName("三国演义");
